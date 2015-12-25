35ab6eeada3a271abcba289218a0c58993464231
justnick <findnick@163.com>
2015-12-25 16:51:19 +0800

    add forecast model

---
+++ NiceLife/Weather/Database/Forecast.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiceLife.Weather.Database
+{
+    public class Forecast
+    {
+        public DateTime date { get; set; }
+        public String hight { get; set; }
+        public String low { get; set; }
+
+        public String dayType { get; set; }
+        public String dayWindDirection { get; set; }
+        public String dayWindPower { get; set; }
+
+        public String nightType { get; set; }
+        public String nightWindDirection { get; set; }
+        public String nightWindPower { get; set; }
+    }
+}
