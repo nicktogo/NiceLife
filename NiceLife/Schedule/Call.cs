294bd0b2478c3d8519674b79bbc8e1f3a14fc8e4
natsukiainsley <xxdainsley@163.com>
2015-12-27 07:12:33 +0800

    update SchedulePage.xaml.cs

---
+++ NiceLife/Schedule/Call.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiceLife.Schedule
+{
+    class Call
+    {
+        public long Id { get; set; }
+        public string Type { get; set; }
+        public string Time { get; set; }
+        public string Sustanin { get; set; }
+    }
+}
