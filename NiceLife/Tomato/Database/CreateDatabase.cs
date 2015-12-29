e4f5fb17c0b4363a7b3e3a7c7e443715b3711792
hhn9549 <hhn9549@163.cm>
2015-12-29 16:29:31 +0800

    Database v1.0
    Database v1.0

---
+++ NiceLife/Tomato/Database/CreateDatabase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLitePCL;
+
+namespace NiceLife.Tomato.Database
+{
+    class CreateDatabase
+    {
+        // create task
+        private static String CREATE_TASK = @"CREATE TABLE IF NOT EXISTS Task (
+            Id integer primary key autoincrement, 
+            Title text, 
+            Description text,
+            Date datetime,
+            Type text,
+            Status text，
+            Total_Tomato integer,
+            Done_Tomato integer
+            );";
+
+        public static void LoadDatabase()
+        {
+            SQLiteConnection conn = new SQLiteConnection(App.DB_NAME);
+            using (var statement = conn.Prepare(CREATE_TASK))
+            {
+                statement.Step();
+            }
+        }
+    }
+}
---
+++ NiceLife/Tomato/Database/DBHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLitePCL;
+
+namespace NiceLife.Tomato
+{
+    public abstract class DBHelper<T>
+    {
+        protected SQLiteConnection conn;
+
+        protected DBHelper()
+        {
+            conn = new SQLiteConnection(App.DB_NAME);
+        }
+
+        public abstract void InsertItems(List<T> items);
+
+        public abstract void InsertSingleItem(T item);
+
+        protected abstract String GetInsertSQL();
+
+        public abstract void DeleteSingleItemById(long id);
+
+        public abstract void UpdateItems(List<T> items);
+
+        public abstract void UpdateSingleItem(T item);
+
+        public abstract List<T> SelectGroupItems(long foreignId);
+
+        protected abstract String GetSelectAllSQL();
+
+        public abstract T SelectSingleItemById(long id);
+
+        protected abstract String GetSelectSQL();
+
+        public abstract T CreateItem(ISQLiteStatement statement);
+    }
+}
