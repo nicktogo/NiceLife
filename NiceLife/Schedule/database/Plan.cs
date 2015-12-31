272c1d20cf22241fc27860193d4de299aefaa279
natsukiainsley <xxdainsley@163.com>
2015-12-31 12:05:15 +0800

    update Plan.cs

---
+++ NiceLife/Schedule/database/Plan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiceLife.Schedule
+{
+    class Plan
+    {
+        public long Id { get; set; }
+        public String Title { get; set; }
+        public long colorId { get; set; }
+        public long callId { get; set; }
+        public string Text { get; set; }
+        public string beginTime { get; set; }
+        public string endTime { get; set; }
+        public int call_YorN { get; set; }
+    }
+}
