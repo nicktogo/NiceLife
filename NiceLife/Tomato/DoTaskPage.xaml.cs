c868258fd01dfa5e1b2d214e8706bdb538411a54
hhn9549 <hhn9549@163.cm>
2016-01-02 01:51:56 +0800

    Tomato Clock v1.0 without Statistics and History!

--- NiceLife/Tomato/DoTaskPage.xaml.cs
+++ NiceLife/Tomato/DoTaskPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using NiceLife.Tomato.Database;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -23,26 +24,59 @@ namespace NiceLife
     /// </summary>
     public sealed partial class DoTaskPage : Page
     {
-        Task task;
-
-        public void fresh()
-        {
-            tb_Title.Text = task.Title;
-            tb_Description.Text = task.Description;
-        }
+        CountDown CountDownClock;
 
         public DoTaskPage()
         {
             this.InitializeComponent();
-
-            
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
-            task = (Task)e.Parameter;
-            fresh();
+            Task task = (Task)e.Parameter;
+            tb_Title.Text = task.Title;
+            tb_Description.Text = task.Description;
+
+            CountDownClock = new CountDown(task);
+            CountDownClock.HorizontalAlignment = HorizontalAlignment.Left;
+            CountDownClock.VerticalAlignment = VerticalAlignment.Center;
+            CountDownClock.Margin = new Thickness(50, 30, 0, 30);
+            g_DoTask.Children.Add(CountDownClock);
+            Grid.SetRow(CountDownClock, 2);
+
+            if (task.Date.ToString("yyyy-MM-dd") != DateTime.Now.ToString("yyyy-MM-dd") || task.Status == "Done")
+            {
+                btn_NewInterrupt.IsEnabled = false;
+            }
+        }
+
+        private void btn_NewInterruptConfirm_Click(object sender, RoutedEventArgs e)
+        {
+            Task task = new Task();
+            task.Title = tb_InterruptTitle.Text;
+            task.Description = tb_InterruptDescription.Text;
+            task.Date = DateTime.Now;
+            if (rb_Internal.IsChecked == true)
+            {
+                task.Type = "Internal";
+            }
+            else
+            {
+                task.Type = "External";
+            }
+            task.Status = "Undone";
+            task.TotalTomato = cb_InterruptTomato.SelectedIndex + 1;
+            task.DoneTomato = 0;
+            TaskHelper.GetHelper().InsertSingleItem(task);
+
+            tb_Title.Text = "";
+            tb_Description.Text = "";
+            rb_Internal.IsChecked = true;
+            cb_InterruptTomato.SelectedIndex = 0;
+
+            this.Frame.Navigate(typeof(DoTaskPage), task);
+            //fo_NewInterrupt.Hide();
         }
     }
 }
