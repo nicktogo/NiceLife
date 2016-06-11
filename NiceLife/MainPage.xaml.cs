26342aad5bbc6b83bb454add2648af5e5fe0fcbf
hxx <793777976@qq.com>
2016-06-11 09:34:41 +0800

    还有一个错感觉不像我的锅

--- NiceLife/MainPage.xaml.cs
+++ NiceLife/MainPage.xaml.cs
@@ -27,43 +27,43 @@ namespace NiceLife
     public sealed partial class MainPage : Page
     {
         List<Call> call;
-       
+
         public MainPage()
         {
-           
+
             this.InitializeComponent();
             check();
         }
-        public  void check()
+        public void check()
         {
-          //  Schedule.db.CreateDb.LoadDatabase();
-                CallHelper callp =CallHelper.GetHelper();
-                call = callp.SelectlistItems(DateTime.Now);
+            //  Schedule.db.CreateDb.LoadDatabase();
+            CallHelper callp = CallHelper.GetHelper();
+            call = callp.SelectlistItems(DateTime.Now);
             for (int i = 0; i < call.Count; i++)
             {
                 PlanHelper ph = PlanHelper.GetHelper();
                 Plan plan = ph.SelectSingleItemById(call.ElementAt(i).PlanId);
 
-                    DateTime toastTime;
-                    try
-                    {
-                        toastTime =call.ElementAt(i).Date;
-                        XmlDocument xdoc = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText01);
+                DateTime toastTime;
+                try
+                {
+                    toastTime = call.ElementAt(i).Date;
+                    XmlDocument xdoc = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText01);
 
-                        var txtnodes = xdoc.GetElementsByTagName("text");
-                        txtnodes[0].InnerText = plan.Title+":"+ plan.Description;
-                       
-                        ScheduledToastNotification toast3 = new ScheduledToastNotification(xdoc, toastTime);
-                        ToastNotificationManager.CreateToastNotifier().AddToSchedule(toast3);
-                    }
-                    catch (Exception ex)
-                    {
-                       //
-                       
-                    }
-                
+                    var txtnodes = xdoc.GetElementsByTagName("text");
+                    txtnodes[0].InnerText = plan.Title + ":" + plan.Description;
+
+                    ScheduledToastNotification toast3 = new ScheduledToastNotification(xdoc, toastTime);
+                    ToastNotificationManager.CreateToastNotifier().AddToSchedule(toast3);
+                }
+                catch (Exception ex)
+                {
+                    //
+
+                }
+
             }
-            
+
         }
         private void BackRadioButton_Click(object sender, RoutedEventArgs e)
         {
@@ -99,15 +99,6 @@ namespace NiceLife
             List<County> counties = countyHelper.GetSelectedItems();
             bool b = counties.Count > 0 ? MainFrame.Navigate(typeof(Weather.WeatherFlipView), Weather.WeatherFlipView.NAVIGATED_FROM_MAINPAGE) : MainFrame.Navigate(typeof(WeatherPage));
         }
-
-        private void KeepRadioButton_Checked(System.Object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void KeepRadioButton_Checked(System.Object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
+
